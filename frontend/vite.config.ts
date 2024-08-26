e66d0e3a28f7aa00baf604b1c393951d839eeb4b
태연정 <june.yeonjung@gmail.com>
2024-08-26 16:33:29 +0900

    commit message

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,18 @@
 import { defineConfig } from 'vite'
-import tsconfigPaths from "vite-tsconfig-paths";
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  server: {
-    host: '0.0.0.0'
+  preview: {
+   port: 80,
+   strictPort: true,
   },
-  base: process.env.NODE_ENV === "development" ? "/" : "./",
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo: any) => {
-          let extType = assetInfo.name.split(".").at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = "img";
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: "assets/js/[name]-[hash].js",
-        entryFileNames: "assets/js/[name]-[hash].js",
-      },
-    },
+  server: {
+   port: 80,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:80",
   },
 });
