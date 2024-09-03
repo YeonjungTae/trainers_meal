619c4233da1364071f2de23cbcdc2047c8b613c6
태연정 <june.yeonjung@gmail.com>
2024-09-04 06:57:52 +0900

    고객 등록 - 배송지, 체성분 필수 X

--- backend/client/client.py
+++ backend/client/client.py
@@ -36,29 +36,60 @@ class ClientClass:
         phone = int(str(request.data['phone']).replace('-', ''))
         gender = get_text_value(Client.Gender, request.data['gender'])
         birthdate = datetime.strptime(str(request.data['birthdate']).replace('.', '-'), '%Y-%m-%d').date()
-        height = round(float(request.data['height']), 2)
+        if request.data['height']:
+            height = round(float(request.data['height']), 2)
+        else:
+            height = float(0)
         activity = request.data['activityLevel']
         goal = request.data['goal']
         notes = request.data['notes']
 
-        weight = round(float(request.data['weight']), 2)
-        skeletal_muscle = round(float(request.data['muscleMass']), 2)
-        body_fat = round(float(request.data['bodyFatMass']), 2)
-        body_fat_ratio = round(float(request.data['bodyFatPercentage']), 2)
-        
-        address = request.data['address']
-        detailAddress = request.data['detailAddress']
-        deliveryMessage = request.data['deliveryMessage']
-        entryMethod = request.data['entryMethod']
-
-        if request.data['entryPassword']:
-            entryPassword = int(request.data['entryPassword'])
-        else:
-            entryPassword = 0
-
         new_client = Client.objects.create(name=name, contact=phone, gender=gender, birth=birthdate, height=height, activity=activity, goal=goal, memo=notes, is_subscribed=False, create_dt=datetime.today(), update_dt=datetime.today(), trainer_id=decode['trainer_id'])
-        Body_Data.objects.create(weight=weight, skeletal_muscle=skeletal_muscle, body_fat=body_fat, body_fat_ratio=body_fat_ratio, update_dt=new_client.update_dt, client_id=new_client.client_id)
-        Delivery.objects.create(address=address, address_detail=detailAddress, message=deliveryMessage, doorlock=entryPassword, doorlock_type=entryMethod, client_id=new_client.client_id)
+
+        if request.data['weight'] or request.data['muscleMass'] or request.data['bodyFatMass'] or request.data['bodyFatPercentage']:
+            if request.data['weight']:
+                weight = round(float(request.data['weight']), 2)
+            else:
+                weight = float(0)
+            if request.data['muscleMass']:
+                skeletal_muscle = round(float(request.data['muscleMass']), 2)
+            else:
+                skeletal_muscle = float(0)
+            if request.data['bodyFatMass']:
+                body_fat = round(float(request.data['bodyFatMass']), 2)
+            else:
+                body_fat = float(0)
+            if request.data['bodyFatPercentage']:
+                body_fat_ratio = round(float(request.data['bodyFatPercentage']), 2)
+            else:
+                body_fat_ratio = float(0)
+
+            Body_Data.objects.create(weight=weight, skeletal_muscle=skeletal_muscle, body_fat=body_fat, body_fat_ratio=body_fat_ratio, update_dt=new_client.update_dt, client_id=new_client.client_id)
+
+        
+        if request.data['address'] or request.data['detailAddress'] or request.data['deliveryMessage'] or entryMethod = request.data['entryMethod']:
+            if request.data['address']:
+                address = request.data['address']
+            else:
+                address = ''
+            if request.data['detailAddress']:
+                detailAddress = request.data['detailAddress']
+            else:
+                detailAddress = ''
+            if request.data['deliveryMessage']:
+                deliveryMessage = request.data['deliveryMessage']
+            else:
+                deliveryMessage = 0
+            if request.data['entryMethod']:
+                entryMethod = request.data['entryMethod']
+            else:
+                entryMethod = 2
+            if request.data['entryPassword']:
+                entryPassword = int(request.data['entryPassword'])
+            else:
+                entryPassword = 0
+
+            Delivery.objects.create(address=address, address_detail=detailAddress, message=deliveryMessage, doorlock=entryPassword, doorlock_type=entryMethod, client_id=new_client.client_id)
 
     def add_bia(request):
         client_id = request.data['clientId']
