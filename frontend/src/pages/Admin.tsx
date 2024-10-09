091711f8e28d3d3367514979bd806fc3bbf851e3
태연정 <june.yeonjung@gmail.com>
2024-10-09 18:14:40 +0900

    edit message

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,5 +1,4 @@
-
-import { apiClient } from "../api";
+import axios from "axios";
 import Button from "../components/ui/Button";
 import styled from "styled-components";
 
@@ -9,14 +8,25 @@ const Admin = () => {
   const handleDownload = async () => {
 
     try {
-      const response = await apiClient.post("/");
+      const response = await axios.post(import.meta.env.VITE_BASE_URL + "/admin/",
+        {responseType: 'blob'}
+      );
 
-      const regx = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+      const a = document.createElement('a');
+      const blob = new Blob([response.data], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      });
 
-      if (regx.test(response.data)) {
-        alert(response.data);
-        return;
-      }
+    //   var FileSaver = require('file-saver');
+    //   FileSaver.saveAs(blob, 'excel.xlsx');
+
+      a.href = window.URL.createObjectURL(blob);
+      a.target = '_blank';
+      a.style.display = 'none';
+      document.body.appendChild(a);
+      a.click();
+      URL.revokeObjectURL(a.href);
+      document.body.removeChild(a);
 
     } catch (error) {
       console.error(error);
