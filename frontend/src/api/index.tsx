e9a80840acc85aeaf31c8ccd886fe5bf03761950
태연정 <june.yeonjung@gmail.com>
2024-10-15 19:52:32 +0900

    excel

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
-  headers: {
-    "Content-Type": `application/json;charset=UTF-8`,
-    "Accept": "application/json",
+  baseURL: import.meta.env.VITE_BASE_URL
+  // headers: {
+  //   "Content-Type": `application/json;charset=UTF-8`,
+  //   "Accept": "application/json",
   
-    // 추가  
-    // "Access-Control-Allow-Origin": `http://3.37.154.71:3000`,
-    // 'Access-Control-Allow-Credentials':"true",
-  },
-  withCredentials: true
+  //   // 추가  
+  //   // "Access-Control-Allow-Origin": `http://3.37.154.71:3000`,
+  //   // 'Access-Control-Allow-Credentials':"true",
+  // },
+  // withCredentials: true
 });
