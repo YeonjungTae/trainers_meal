71ca5d9094e59cd5be749dfb71dc46eef92d830b
태연정 <june.yeonjung@gmail.com>
2024-10-15 19:38:48 +0900

    excel

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -7,7 +7,7 @@ export const apiClient = axios.create({
     "Accept": "application/json",
   
     // 추가  
-    "Access-Control-Allow-Origin": `http://3.37.154.71:3000`,
+    // "Access-Control-Allow-Origin": `http://3.37.154.71:3000`,
     'Access-Control-Allow-Credentials':"true",
   },
   withCredentials: true
